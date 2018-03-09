a51c9c915e5e248c2cacecea747c8e06957b32eb
igna84 <igna84@gmail.com>
2018-03-09 14:55:27 +0900

    기본 설정 내용 변경. 최초 프로젝트 생성시 프로젝트 내 settings 디렉토리 base.py 파일의 project_name 을 프로젝트 명으로 치환 바람

--- default_settings/production.py
+++ default_settings/production.py
@@ -1,2 +1,16 @@
 from .base import *
 
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
+}
--- default_settings/test.py
+++ default_settings/test.py
@@ -2,3 +2,17 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
+}
